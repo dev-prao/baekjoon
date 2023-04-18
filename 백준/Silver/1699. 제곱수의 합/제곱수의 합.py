d09694ae313b7854eb874815f7d3e680c25cad51
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-04-18 19:56:34 +0900

    [Silver II] Title: 제곱수의 합, Time: 5536 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -1,11 +1,10 @@
-import math
-
 n = int(input())
-dp = [float('inf')] * (n + 1)
-dp[0] = 0
-
+d = [0] * (n + 1)
 for i in range(1, n+1):
-    for j in range(1, int(math.sqrt(i))+1):
-        dp[i] = min(dp[i], dp[i-j*j] + 1)
-
-print(dp[n])
+    d[i] = i
+    j = 1
+    while j * j <= i:
+        if d[i] > d[i - j * j] + 1:
+            d[i] = d[i - j * j] + 1
+        j += 1
+print(d[n])
