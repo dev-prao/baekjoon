31e58b8c0503c7f7edba52f436360fdd05cb4eb5
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-05-25 23:12:05 +0900

    [Bronze III] Title: 직각삼각형, Time: 120 ms, Memory: 13976 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,33 +1,34 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
+    
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
+        StringBuilder sb = new StringBuilder();
+        
         while(true) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
             
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int z = Integer.parseInt(st.nextToken());
             
-            if(x==0 && y==0 && z==0) break;
+            if(x == 0 && y == 0 && z== 0) break;
             
-            if(x*x + y*y == z*z) {
-                System.out.println("right");
-            } else if(z*z + y*y == x*x) {
-                System.out.println("right");
-            } else if(x*x + z*z == y*y) {
-                System.out.println("right");
+            if((x * x + y * y) == z * z) {
+                sb.append("right").append('\n');
+            } else if((z * z + y * y) == x * x) {
+                sb.append("right").append('\n');
+            } else if((x * x + z * z) == y * y) {
+                sb.append("right").append('\n');
             } else {
-                System.out.println("wrong");
+                sb.append("wrong").append('\n');
             }
-            
         }
-       
-        
-    }
+        System.out.println(sb);        
+    }   
 }
