543763b85b26aa9b2b5dc3404cb8dca20837d3b1
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-06-11 23:06:46 +0900

    [Silver V] Title: 단어 정렬, Time: 80 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,14 +1,19 @@
 import sys
 
-n = int(sys.stdin.readline())
-lst = []
+# 입력 함수를 sys.stdin.readline으로 설정
+input = sys.stdin.readline
 
+n = int(input())
+lst = set()
+
+# 한 줄씩 입력을 받아서 집합에 추가
 for i in range(n):
-    lst.append(sys.stdin.readline().strip())
-set_lst = set(lst)
-lst = list(set_lst)
-lst.sort()
-lst.sort(key = len)
+    word = input().strip()
+    lst.add(word)
 
-for i in lst:
-    print(i)
+# 집합을 정렬한 후 길이순으로 정렬하여 리스트로 변환
+lst = sorted(sorted(lst), key=len)
+
+# 리스트의 원소를 한 줄씩 출력
+for word in lst:
+    print(word)
