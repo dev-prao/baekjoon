6149ae884b9629d8810ce884ffc0de7bc72a527e
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-06-11 19:38:02 +0900

    [Bronze V] Title: 카드 게임, Time: 216 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int n;
+        for(int i = 0; i < 5; i++) {
+            n = sc.nextInt();
+            sum += n;
+        }
+        sc.close();
+        System.out.print(sum);
+    }
+}
