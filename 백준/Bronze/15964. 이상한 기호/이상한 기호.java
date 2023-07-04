0dd78c639953b75ae25f860747f3d6868fdc4a23
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-07-04 23:09:56 +0900

    [Bronze V] Title: 이상한 기호, Time: 208 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -5,6 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
+        sc.close();
         System.out.println((A+B)*(A-B));
     }
 }
