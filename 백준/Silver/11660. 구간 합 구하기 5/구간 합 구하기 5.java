48390c81b427790c71dbdd86798aad449d075843
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-07-06 12:42:18 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1800 ms, Memory: 127840 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,37 +1,49 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        // 첫째줄 - 표의 크기 n, 합을 구해야 하는 횟수 m
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken()); // 표의 크기
-        int m = Integer.parseInt(st.nextToken()); // 질의 갯수
-
-        int A[][] = new int[n + 1][n + 1];
+        // 둘째줄부터 n개의 줄
+        int arr[][] = new int[n + 1][n + 1];
         for (int i = 1 ; i <= n ; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1 ; j <= n ; j++) {
-                A[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int D[][] = new int[n + 1][n + 1];
-        for (int i = 1 ; i <= n ; i++) {
-            for (int j = 1 ; j <= n ; j++) {
-                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
+
+        // 구간 합 구하기
+        int arrSum[][] = new int[n + 1][n + 1];
+        arrSum[1][1] = arr[1][1];
+        for (int i = 2 ; i <= n ; i++) {
+            arrSum[1][i] = arrSum[1][i - 1] + arr[1][i];
+        }
+        for (int i = 2 ; i <= n ; i++) {
+            arrSum[i][1] = arrSum[i-1][1] + arr[i][1];
+        }
+        for (int i = 2 ; i <= n ; i++) {
+            for (int j = 2 ; j <= n ; j++) {
+                arrSum[i][j] = arrSum[i-1][j] + arrSum[i][j-1] + arr[i][j] - arrSum[i-1][j-1];
             }
         }
-        for (int i = 1 ; i <= m ; i++) {
+
+        // m번 만큼 질의에 대한 값 출력
+        for (int i = 0 ; i < m ; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            System.out.println(D[x2][y2] - D[x2][y1 - 1] - D[x1 - 1][y2] + D[x1 - 1][y1 - 1]);
-        }
 
+            System.out.println(arrSum[x2][y2] - arrSum[x2][y1 - 1] - arrSum[x1 - 1][y2] + arrSum[x1 - 1][y1 - 1]);
+        }
     }
 }
