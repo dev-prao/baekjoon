ac1b2dbf8c26bdf00821793c1f9e507bbf70f98e
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-07-17 13:00:04 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,25 +1,27 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int sSum = 0;
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        br.close();
+        int sum = 0;
         int maxNum = 0;
-        int sArr[] = new int[N];
-        for (int i = 0 ; i < N ; i++) {
-            sArr[i] = Integer.parseInt(st.nextToken());
-            if (sArr[i] > maxNum) {
-                maxNum = sArr[i];
-            }
-            sSum += sArr[i];
-        }
-        System.out.println(sSum * 100.0 / (maxNum * N));
+        int numArr[] = new int[n];
 
+        for (int i = 0 ; i < n ; i++) {
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0 ; i < n ; i++) {
+            sum += numArr[i];
+            if(numArr[i] > maxNum) {
+                maxNum = numArr[i];
+            }
+        }
+
+        System.out.println((double)sum * 100 / (maxNum*n));
     }
 }
