b52c7ed85b6c6d9173e335f6cc4cc235fa9fffc6
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-07-22 16:21:33 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            String level = "";
+            if (name == "#" || age == 0 || weight == 0) break;
+            if (age > 17 || weight >= 80) {
+                level = "Senior";
+            } else {
+                level = "Junior";
+            }
+            System.out.printf("%s %s\n", name, level);
+        }
+    }
+}
