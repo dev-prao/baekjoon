d9c4d26688ee0a12b7e814483d3f21d7e9be9943
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-08-09 17:09:55 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,22 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        for (int a = 0 ; a < commands.length ; a++) {
-            int i = commands[a][0] - 1;
-            int j = commands[a][1] - 1;
-            int k = commands[a][2] - 1;
-
-            int[] newArr = new int[j - i + 1];
-            int idx = 0;
-            for (int l = i ; l <= j ; l++) {
-                newArr[idx] = array[l];
-                idx++;
-            }
-            Arrays.sort(newArr);
-            answer[a] = newArr[k];
-        }
+        int[] answer = {};
         return answer;
     }
 }
