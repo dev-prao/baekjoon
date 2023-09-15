1a5153dbe20aafadef6728c977e29904f1d8fbf8
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-09-16 03:52:44 +0900

    [Bronze V] Title: CAPS, Time: 532 ms, Memory: 30948 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String in = sc.nextLine();
+        String out = in.toUpperCase();
+        System.out.println(out);
+        sc.close();
+    }
+}
