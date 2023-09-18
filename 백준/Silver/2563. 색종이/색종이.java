bca9d3c3047236665eaf8287fc8685744bd2a1f6
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-09-18 14:18:31 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -6,31 +6,26 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[][] arr = new int[101][101];
-		int result = 0;
+		int blackArea = 0; // 검은 영역의 넓이
+		int n = Integer.parseInt(br.readLine()); // 색종이 개수
+		boolean[][] canvas = new boolean[101][101]; // 도화지
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			int maxX = x > 90 ? 100 : x + 10;
-			int maxY = y > 90 ? 100 : y + 10;
-			for (int j = x + 1; j <= maxX; j++) {
-				for (int k = y + 1; k <= maxY; k++) {
-					arr[j][k] = 1;
+
+			// (x, y)부터 (x+9, y+9)까지의 영역을 하나씩 체크하여 검은 영역을 더해줍니다.
+			for (int j = x; j < x + 10; j++) {
+				for (int k = y; k < y + 10; k++) {
+					if (!canvas[j][k]) {
+						canvas[j][k] = true;
+						blackArea++;
+					}
 				}
 			}
 		}
-		br.close();
-		
-		for (int i = 0; i <= 100; i++) {
-			for (int j = 0; j <= 100; j++) {
-				if (arr[i][j] == 1) {
-					result++;
-				}
-			}
-		}
-		System.out.println(result);
+
+		System.out.print(blackArea);
 	}
 }
