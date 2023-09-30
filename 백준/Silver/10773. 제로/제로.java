5c74260ade937ca7ce4a56eca4fa37ef86ab8e53
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-09-30 14:23:01 +0900

    [Silver IV] Title: 제로, Time: 252 ms, Memory: 23232 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int K = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < K; i++) {
+			int number = Integer.parseInt(br.readLine());
+			if (number != 0) {
+				stack.push(number);
+			}
+			if (number == 0) {
+				stack.pop();
+			}
+		}
+		br.close();
+
+		int answer = 0;
+		while (!stack.isEmpty()) {
+			answer += stack.pop();
+		}
+		System.out.println(answer);
+	}
+}
