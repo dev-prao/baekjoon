0688400262405760657ca3025661e1044f675989
prao <101629085+p-r-a-o@users.noreply.github.com>
2023-10-04 16:34:02 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int price = Integer.parseInt(br.readLine());
+		br.close();
+		System.out.println(price * 10 / 11);
+	}
+}
