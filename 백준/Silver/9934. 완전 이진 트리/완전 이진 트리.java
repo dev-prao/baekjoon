5e338e7259fc33791f68903b701ab5fa0adbd7ed
prao <101629085+dev-prao@users.noreply.github.com>
2024-01-18 10:14:27 +0900

    [Silver I] Title: 완전 이진 트리, Time: 156 ms, Memory: 15824 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.java
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.java
@@ -1,46 +1,28 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	static int K;
-	static int[] arr;
-	static StringBuilder[] answer;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		K = Integer.parseInt(br.readLine());
-		arr = new int[(int)Math.pow(2, K) - 1];
-		
-
-		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        answer = Arrays.stream(arr)
-                .mapToObj(StringBuilder::new)
-                .toArray(StringBuilder[]::new);
-        
-        dfs(0, arr.length - 1, 0);
-        
-        for(int i = 0; i < K; i++) {
-        	bw.write(answer[i] + "\n");
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int k = Integer.parseInt(br.readLine());
+        int[] arr = new int[(int)Math.pow(2, k) - 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        bw.flush();
-        bw.close();
-        br.close();
-	}
-	
-	private static void dfs(int start, int end, int level) {
-		if(level == K) return;
-		int mid = (start + end) / 2;
-		answer[level].append(arr[mid] + " ");
-		
-		dfs(start, mid - 1, level + 1);
-		dfs(mid + 1, end, level + 1);
-	}
-	
+        int size = arr.length;
+        int parent = size;
+        while (parent != 0) {
+            int child = (parent - 1) / 2;
+            int gap = (parent - child) * 2;
+            for(int i = child; i < size; i += gap){
+                sb.append(arr[i] + " ");
+            }
+            parent = child;
+            sb.append("\n");
+        }
+        System.out.println(sb);
+				br.close();
+    }
 }
