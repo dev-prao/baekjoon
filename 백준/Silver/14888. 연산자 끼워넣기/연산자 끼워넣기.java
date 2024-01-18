51fcf8dfead1dfbc6400b55bc30d1863290db7f3
prao <101629085+dev-prao@users.noreply.github.com>
2024-01-18 09:58:41 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 176 ms, Memory: 19364 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -2,21 +2,21 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	public static int MAX = Integer.MIN_VALUE;	
+
+	public static int MAX = Integer.MIN_VALUE;
 	public static int MIN = Integer.MAX_VALUE;
 	public static int[] operator = new int[4];
 	public static int[] number;
 	public static int N;
- 
+
 	public static void main(String[] args) throws IOException {
- 
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		number = new int[N];
- 
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			number[i] = Integer.parseInt(st.nextToken());
@@ -26,41 +26,44 @@ public class Main {
 		for (int i = 0; i < 4; i++) {
 			operator[i] = Integer.parseInt(st.nextToken());
 		}
- 
+
 		calculate(number[0], 1);
- 
-		System.out.println(MAX + "\n" + MIN);		
+
+		System.out.println(MAX + "\n" + MIN);
 		br.close();
 	}
- 
+
 	public static void calculate(int num, int index) {
 		if (index == N) {
 			MAX = Math.max(MAX, num);
 			MIN = Math.min(MIN, num);
 			return;
 		}
- 
+
 		for (int i = 0; i < 4; i++) {
 			if (operator[i] > 0) {
- 
+
 				operator[i]--;
-				
-				if(i == 0) {
+
+				switch (i) {
+				case 0:
 					calculate(num + number[index], index + 1);
-				}
-				if(i == 1) {
+					break;
+				case 1:
 					calculate(num - number[index], index + 1);
-				}
-				if(i == 2) {
+					break;
+				case 2:
 					calculate(num * number[index], index + 1);
-				}
-				if(i == 3) {
+					break;
+				case 3:
 					calculate(num / number[index], index + 1);
+					break;
+
 				}
-				
+
 				operator[i]++;
 			}
 		}
 	}
- 
+
 }
