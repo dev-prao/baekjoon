11b3f32ccb5f7833f2829b7a77cf30dd9586066b
prao <101629085+dev-prao@users.noreply.github.com>
2024-01-20 16:45:49 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10344 ms, Memory: 305780 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +14,6 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
@@ -50,11 +47,9 @@ public class Main {
 
 		for (int i = 1; i < N + 1; i++) {
 			if (answer[i] == max) {
-				bw.write(i + " ");
+				System.out.print(i + " ");
 			}
 		}
-		bw.flush();
-		bw.close();
 		br.close();
 	}
 
