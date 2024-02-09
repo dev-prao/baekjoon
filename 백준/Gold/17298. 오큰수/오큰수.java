26221535ce82bef6f18851a0d3b13969eadbc030
prao <101629085+dev-prao@users.noreply.github.com>
2024-02-09 22:40:06 +0900

    [Gold IV] Title: 오큰수, Time: 824 ms, Memory: 197016 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		Deque<Integer> stack = new ArrayDeque<>();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < n; i++) {
+			while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+				arr[stack.pop()] = arr[i];
+			}
+			stack.push(i);
+		}
+
+		while (!stack.isEmpty()) {
+			arr[stack.pop()] = -1;
+		}
+
+		for (int i : arr) {
+			sb.append(i).append(" ");
+		}
+
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+	}
+}
