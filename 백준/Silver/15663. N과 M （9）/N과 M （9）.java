a31d1a844a45057c2a2eea62bf9ef1b0ab6c0150
prao <101629085+dev-prao@users.noreply.github.com>
2024-02-25 00:36:42 +0900

    [Silver II] Title: N과 M (9), Time: 852 ms, Memory: 40704 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,68 +1,48 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
 
 public class Main {
-
 	static int N, M;
-	static int[] arr;
-	static int[] printArr;
+	static int[] arr, printArr;
 	static boolean[] visit;
-	static List<String> p;
-	static StringBuilder sb = new StringBuilder();
+	static LinkedHashSet<String> ans;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+
 		arr = new int[N];
-		visit = new boolean[N];
 		printArr = new int[M];
-		p = new ArrayList<>();
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+		visit = new boolean[N];
+		ans = new LinkedHashSet<>();
+
+		for (int i = 0; i < N; i++)
+			arr[i] = sc.nextInt();
 
 		Arrays.sort(arr);
-
 		dfs(0);
-		for (String s : p) {
-			sb.append(s).append("\n");
-		}
-		bw.write(sb.toString());
-		bw.close();
-		br.close();
+		ans.forEach(System.out::println);
 	}
 
 	static void dfs(int depth) {
 		if (depth == M) {
-			StringBuilder s = new StringBuilder();
-			for (int i : printArr) {
-				s.append(i).append(" ");
+			StringBuilder sb = new StringBuilder();
+			for (int p : printArr) {
+				sb.append(p).append(' ');
 			}
-			if (!p.contains(s.toString())) {
-				p.add(s.toString());
-			}
+			ans.add(sb.toString());
 			return;
 		}
 
 		for (int i = 0; i < N; i++) {
 			if (!visit[i]) {
-				printArr[depth] = arr[i];
 				visit[i] = true;
+				printArr[depth] = arr[i];
 				dfs(depth + 1);
 				visit[i] = false;
 			}
 		}
 	}
-
 }
