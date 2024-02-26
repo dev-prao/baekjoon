875454ddc23877d2a2079af62f16380ff910d114
prao <101629085+dev-prao@users.noreply.github.com>
2024-02-26 14:14:30 +0900

    [Gold V] Title: 이진 검색 트리, Time: 672 ms, Memory: 33392 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Node root = new Node(Integer.parseInt(br.readLine()));
+        String input;
+        while (true) {
+            input = br.readLine();
+            if (input == null || input.equals(""))
+                break;
+            root.insert(Integer.parseInt(input));
+        }
+
+        postOrder(root);
+    }
+
+    static void postOrder(Node node) {
+        if (node == null)
+            return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.data);
+    }
+    
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int num) {
+            this.data = num;
+        }
+
+        void insert(int n) {
+            if (n < this.data) {
+                if (this.left == null)
+                    this.left = new Node(n);
+                else this.left.insert(n);
+            } else {
+                if (this.right == null)
+                    this.right = new Node(n);
+                else this.right.insert(n);
+            }
+        }
+    }
+}
