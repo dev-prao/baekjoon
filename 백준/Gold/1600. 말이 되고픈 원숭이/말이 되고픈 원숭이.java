1e65e4daa8df6000030a8334679a397c3ab91030
prao <101629085+dev-prao@users.noreply.github.com>
2024-04-04 14:26:00 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 408 ms, Memory: 53656 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -27,61 +27,57 @@ public class Main {
         }
 
         int[][] isVisited = new int[h][w];
-        for(int r = 0; r < h; r++) {
-            Arrays.fill(isVisited[r], 987654321);
-        }
+
         Deque<int[]> q = new ArrayDeque<int[]>();
-        isVisited[0][0] = 10;
-        q.offer(new int[] { 0, 0, 0, 10 });
-        int ans = -1;
 
-        while (ans == -1 && !q.isEmpty()) {
-            int curIter = q.size();
+        isVisited[0][0] = 1;
+        q.offer(new int[] { 1, 0, 0 });
+        int ans = -1; // depth를 의미
 
-            while (curIter-- > 0) {
+        while (!q.isEmpty()) {
+
+            // 들어갈 때마다 moveCnt 증가하는 셈.
+            // > 1 depth 증가
+            ans++;
+            int qSize = q.size();
+            for (int i = 0; i < qSize; i++) {
                 int[] curPos = q.poll();
                 int curJump = curPos[0];
                 int curR = curPos[1];
                 int curC = curPos[2];
-                int moveCnt = curPos[3];
 
                 if (curR == h - 1 && curC == w - 1) {
-                    ans = moveCnt - 10;
-                    break;
+                    System.out.println(ans);
+                    br.close();
+                    return;
                 }
 
-                for (int d = 0; d < 4; d++) {
+                for (int d = 0; d < 12; d++) {
+                    // 말 이동 횟수 없으면 4방향 탐색까지만
+                    if (d > 3 && curJump == k + 1)
+                        break;
+
                     int nr = curR + dr[d];
                     int nc = curC + dc[d];
+                    int tempJump = curJump;
+                    if (d >= 4)
+                        tempJump++;
 
-                    if (curJump <= k && nr >= 0 && nr < h && nc >= 0 && nc < w && map[nr][nc] == 0 && curJump < isVisited[nr][nc]) {
-                        isVisited[nr][nc] = curJump;
-                        q.offer(new int[] { curJump, nr, nc, moveCnt+1 });
+                    // 범위 초과 시
+                    if (nr < 0 || nc < 0 || nr >= h || nc >= w)
+                        continue;
+                    // 장애물 or 말 이동 횟수 덜 사용하고 이미 방문 시
+                    // > 이미 방문했다면 0보단 커야함!
+                    if (map[nr][nc] == 1 || (isVisited[nr][nc] > 0 && isVisited[nr][nc] <= tempJump)) {
+                        continue;
                     }
-                }
-
-                for (int d = 4; d < 12; d++) {
-                    int nr = curR + dr[d];
-                    int nc = curC + dc[d];
-
-                    if (curJump < k && nr >= 0 && nr < h && nc >= 0 && nc < w && map[nr][nc] == 0 && curJump + 1 < isVisited[nr][nc]) {
-                        isVisited[nr][nc] = curJump + 1;
-                        q.offer(new int[] { curJump + 1, nr, nc, moveCnt + 1 });
-                    }
+                    isVisited[nr][nc] = tempJump;
+                    q.offer(new int[] { tempJump, nr, nc });
                 }
             }
         }
-        
-//        for(int r = 0; r < h; r++) {
-//            for(int c = 0; c < w; c++) {
-//                int res = isVisited[r][c];
-//                res = res == Integer.MAX_VALUE ? 0 : res;
-//                System.out.printf("%2d ", res);
-//            }
-//            System.out.println();
-//        }
 
-        System.out.println(ans);
+        System.out.println(-1);
         br.close();
     }
 
