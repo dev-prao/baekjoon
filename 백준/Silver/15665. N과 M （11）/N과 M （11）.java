cbab28d295418d57904340ab24dfa613dd52a85f
prao <101629085+dev-prao@users.noreply.github.com>
2024-06-05 14:45:37 +0900

    [Silver II] Title: N과 M (11), Time: 316 ms, Memory: 45036 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -3,46 +3,47 @@ import java.io.*;
 
 public class Main {
 
-	static int N, M;
-	static int[] arr, printArr;
-	static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] arr, printArr;
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		N = sc.nextInt();
-		M = sc.nextInt();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		arr = new int[N];
-		printArr = new int[M];
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
-		}
+        arr = new int[N];
+        printArr = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		Arrays.sort(arr);
+        Arrays.sort(arr);
 
-		dfs(0);
-		System.out.println(sb);
-	}
+        dfs(0);
+        System.out.println(sb);
+    }
 
-	static void dfs(int depth) {
-		if (depth == M) {
-			for (int i = 0; i < M; i++) {
-				sb.append(printArr[i]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(printArr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
 
-		int before = -1;
-		for (int i = 0; i < N; i++) {
-			int now = arr[i];
-			if (before != now) {
-				before = now;
-				printArr[depth] = arr[i];
-				dfs(depth + 1);
-			}
-
-		}
-	}
+        int before = -1;
+        for (int i = 0; i < N; i++) {
+            int now = arr[i];
+            if (before != now) {
+                before = now;
+                printArr[depth] = arr[i];
+                dfs(depth + 1);
+            }
+        }
+    }
 }
