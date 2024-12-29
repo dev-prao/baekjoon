187a38663974755a6564c6b84968efd193085132
prao <101629085+dev-prao@users.noreply.github.com>
2024-12-29 17:18:05 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 240 ms, Memory: 35976 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,73 +1,62 @@
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int[] dr = {-1, 0, 0, 1}; // 상, 좌, 우, 하
-	static int[] dc = {0, -1, 1, 0};
-	static boolean[][] visited;
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 
+		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
 		char[][] campus = new char[N][M];
-		visited = new boolean[N][M]; // 방문 체크 배열
-		Point doyeon = new Point(0, 0);
+		boolean[][] visited = new boolean[N][M];
+		int[] start = new int[2];
 
-		// 캠퍼스 정보 입력 및 도연 위치 찾기
 		for (int i = 0; i < N; i++) {
-			String input = br.readLine();
+			campus[i] = br.readLine().toCharArray();
 			for (int j = 0; j < M; j++) {
-				campus[i][j] = input.charAt(j);
 				if (campus[i][j] == 'I') {
-					doyeon = new Point(i, j); // 도연의 시작 위치
-				}
+					start[0] = i;
+					start[1] = j;
+				} // 도연이 위치 찾음
 			}
 		}
 
-		// DFS 수행
-		int result = dfs(doyeon, campus);
+		int[] dr = {-1, 0, 1, 0};
+		int[] dc = {0, 1, 0, -1};
 
-		// 출력
-		if (result == 0) {
-			System.out.println("TT"); // 아무도 만나지 못한 경우
-		} else {
-			System.out.println(result); // 만난 사람의 수 출력
-		}
-	}
-
-	static int dfs(Point current, char[][] campus) {
 		int count = 0;
+		Queue<int[]> queue = new LinkedList<>();
+		visited[start[0]][start[1]] = true;
+		queue.add(start);
 
-		// 현재 위치 방문 처리
-		visited[current.x][current.y] = true;
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
 
-		// 현재 위치가 사람(P)인 경우 카운트 증가
-		if (campus[current.x][current.y] == 'P') {
-			count++;
-		}
-
-		// 4방향 탐색
-		for (int d = 0; d < 4; d++) {
-			int nr = current.x + dr[d];
-			int nc = current.y + dc[d];
-
-			// 범위 체크 및 방문 여부 확인
-			if (nr >= 0 && nc >= 0 && nr < campus.length && nc < campus[0].length && !visited[nr][nc]) {
-				// 벽(X)이 아닌 경우만 탐색
-				if (campus[nr][nc] != 'X') {
-					count += dfs(new Point(nr, nc), campus); // 재귀 호출
+			for (int i = 0; i < 4; i++) {
+				int nr = curr[0] + dr[i];
+				int nc = curr[1] + dc[i];
+				if (nr < 0 || nc < 0 || nr >= N || nc >= M || visited[nr][nc]
+					|| campus[nr][nc] == 'X') {
+					continue;
 				}
+
+				if (campus[nr][nc] == 'P') {
+					count++;
+				}
+				visited[nr][nc] = true;
+				queue.add(new int[] {nr, nc});
 			}
 		}
 
-		return count;
+		if (count > 0) {
+			System.out.println(count);
+		} else {
+			System.out.println("TT");
+		}
 	}
+
 }
